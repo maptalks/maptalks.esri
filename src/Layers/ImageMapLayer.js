b952eafeb5fdbdd3494ad4faf67537d0d5da10fe
fuzhenn <fuzhen@maptalks.org>
2022-04-25 16:04:55 +0800

    fix ImageMapLayer

--- src/Layers/ImageMapLayer.js
+++ src/Layers/ImageMapLayer.js
@@ -4,10 +4,8 @@
  */
 import * as maptalks from 'maptalks';
 import ImageService from './../Services/ImageService';
-import merge from './../Utils/merge';
-import Delaunay from './../Utils/delaunay';
 
-const _options = {
+const options = {
     renderer: maptalks.Browser.webgl ? 'gl' : 'canvas',
     updateInterval: 150,
     format: 'jpgpng',
@@ -15,18 +13,20 @@ const _options = {
     f: 'image',
 };
 
-export default class ImageMapLayer extends maptalks.Layer {
+const TEMP_COORD = new maptalks.Coordinate(0, 0);
+
+export default class ImageMapLayer extends maptalks.ImageLayer {
 
     constructor(id, options = {}) {
-        super(id, options);
-        this._options = merge({}, _options, options);
+        super(id, [], options);
         this._url = options.url;
         this._service = new ImageService(this._url);
+        this._esriImages = [];
     }
 
     getEvents() {
         return {
-            'zoomend': function () {
+            'zoomend moveend rotateend': function () {
                 this._reLoad();
             }
         }
@@ -37,226 +37,61 @@ export default class ImageMapLayer extends maptalks.Layer {
         const params = this._buildExportParams();
         this._service.exportImage(params).then(resp => {
             //获取结果图片地址
-            this._cacheData = JSON.parse(resp);
-            this._width = this._cacheData.width;
-            this._height = this._cacheData.height;
-            this._buildDelaunay();
-            this.load();
+            const image = JSON.parse(resp);
+            image.url = image.href;
+            const extent = image.extent;
+            const projection = this.getMap().getProjection();
+            const e = new maptalks.Extent(extent).convertTo(c => projection.unproject(c, TEMP_COORD));
+            image.extent = [e.xmin, e.ymin, e.xmax, e.ymax];
+            this._esriImages[0] = image;
+            this.setImages(this._esriImages);
+            const renderer = this.getRenderer();
+            if (renderer) {
+                renderer.setToRedraw();
+            }
         }, err => {
             console.log(err);
         });
-        return false;
+        return true;
     }
 
-    onLoad() {
-        if (this.cacheData) {
-            return true;
-        }
+    onAdd() {
         this._reLoad();
+        super.onAdd();
     }
 
-    get cacheData() {
-        return this._cacheData;
-    }
-
-    get tin() {
-        return {
-            w: this._width,
-            h: this._height,
-            vertices: this._vertices,
-            triangles: this._triangles
-        };
-    }
 
     _buildExportParams() {
         const params = {},
             map = this.getMap(),
-            glZoom = map.getZoom(),
-            geoExtent = map.getExtent(),
-            mapSize = map.getSize();
-        //
-        params.bbox = this._options.extent || [geoExtent.xmin, geoExtent.ymin, geoExtent.xmax, geoExtent.ymax];
+            mapExtent = map.getExtent();
+        const paramExtent = this.options.extent && new maptalks.Extent(this.options.extent);
+
+        const extent = paramExtent ? paramExtent.intersection(mapExtent) : mapExtent;
+
+        params.bbox = [extent.xmin, extent.ymin, extent.xmax, extent.ymax];
         //计算size
         const min = new maptalks.Coordinate(params.bbox[0], params.bbox[1]);
-        const ptMin = map.coordinateToPoint(min, glZoom);
+        const ptMin = map.coordToContainerPoint(min);
         const max = new maptalks.Coordinate(params.bbox[2], params.bbox[3]);
-        const ptMax = map.coordinateToPoint(max, glZoom);
+        const ptMax = map.coordToContainerPoint(max);
         const w = Math.floor(ptMax.x - ptMin.x),
             h = Math.abs(Math.floor(ptMax.y - ptMin.y));
         params.size = w + ',' + h;
-        params.format = this._options.format;
-        params.transparent = this._options.transparent;
+        params.format = this.options.format;
+        params.transparent = this.options.transparent;
+        const projection = map.getProjection();
         //}{axmand debug
         params.bboxSR = '4326';
         //强制要求是4326
-        params.imageSR = '4326';
+        const code = projection.code;
+        params.imageSR = code.substring('EPSG:'.length);
         return params;
     }
-
-    _buildDelaunay() {
-        const vertices = new Array(48),
-            w = this._width,
-            h = this._height;
-        let i, x, y;
-        for (i = vertices.length; i--;) {
-            do {
-                x = Math.random() - 0.5;
-                y = Math.random() - 0.5;
-            } while (x * x + y * y > 0.25);
-            x = (x * 0.96875 + 0.5) * w;
-            y = (y * 0.96875 + 0.5) * h;
-            vertices[i] = [x, y];
-        }
-        const triangles = Delaunay.triangulate(vertices);
-        //更新vertices
-        for (i = vertices.length; i--;) {
-            vertices[i] = vertices[i].concat(Math.random() * 30);
-        }
-        this._vertices = vertices;
-        this._triangles = triangles;
-    }
-
 }
 
+ImageMapLayer.mergeOptions(options);
 
-class ImageCanvasRenderer extends maptalks.renderer.CanvasRenderer {
+ImageMapLayer.registerRenderer('canvas', maptalks.renderer.ImageLayerCanvasRenderer);
+ImageMapLayer.registerRenderer('gl', maptalks.renderer.ImageLayerGLRenderer);
 
-
-    onAdd() {
-
-    }
-
-    draw() {
-        this.prepareCanvas();
-        this._drawImage();
-    }
-
-    _drawImage() {
-        const imgObj = this.layer.cacheData,
-            nw = new maptalks.Coordinate(imgObj.extent.xmin, imgObj.extent.ymax);
-        if (imgObj) {
-            const img = new Image();
-            img.onload = () => {
-                const pt = this.getMap()._prjToContainerPoint(nw);
-                this.context.drawImage(img, pt.x, pt.y);
-                this.completeRender();
-            };
-            img.src = imgObj.href;
-        }
-    }
-
-    drawOnInteracting() {
-        //this.draw()
-        //交互时的绘制，无需请求新的图片，直接绘制上一次获取到的map image
-    }
-
-    hitDetect() {
-        return false;
-    }
-
-    //事件结束后的回调函数
-
-    onZoomEnd() {
-        super.onZoomEnd();
-    }
-
-    onMoveEnd() {
-        super.onMoveEnd();
-    }
-
-    onDragRotateEnd() {
-        super.onDragRotateEnd();
-    }
-
-}
-
-ImageMapLayer.registerRenderer('canvas', ImageCanvasRenderer);
-
-ImageMapLayer.registerRenderer('gl', class extends maptalks.renderer.ImageGLRenderable(ImageCanvasRenderer) {
-
-    draw() {
-        this.prepareCanvas();
-        this._drawImage();
-    }
-
-    _drawImage() {
-        const imgObj = this.layer.cacheData,
-            that = this;
-        const map = this.getMap(),
-            glZoom = map.getGLZoom(),
-            glScale = map.getGLScale(),
-            vertices = this.layer.tin.vertices,
-            triangles = this.layer.tin.triangles,
-            w = this.layer.tin.w,
-            h = this.layer.tin.h,
-            len = vertices.length;
-
-        if (imgObj) {
-            const img = new Image();
-            img.crossOrigin = 'anonymous';
-            img.onload = function () {
-                const nw = new maptalks.Coordinate(imgObj.extent.xmin, imgObj.extent.ymax);
-                const pt = that.layer.getMap().coordinateToPoint(nw, glZoom);
-                const vtcs = [],
-                    txcoord = [];
-                for (let i = 0; i < len; i++) {
-                    //1.build vertices
-                    vtcs.push(pt.x + vertices[i][0] * glScale);
-                    vtcs.push(pt.y + vertices[i][1] * glScale);
-                    vtcs.push(vertices[i][2]);
-                    //2.build txcoord
-                    txcoord.push(vertices[i][0] / w);
-                    txcoord.push(vertices[i][1] / h);
-                }
-                that.drawTinImage(img, vtcs, txcoord, triangles, 1);
-                that.completeRender();
-            };
-            img.src = imgObj.href;
-        }
-    }
-
-    drawOnInteracting() {
-        //this.draw()
-        //交互时的绘制，无需请求新的图片，直接绘制上一次获取到的map image
-    }
-
-    onCanvasCreate() {
-        this.prepareGLCanvas();
-    }
-
-    resizeCanvas(canvasSize) {
-        if (!this.canvas) {
-            return;
-        }
-        super.resizeCanvas(canvasSize);
-        this.resizeGLCanvas();
-    }
-
-    clearCanvas() {
-        if (!this.canvas) {
-            return;
-        }
-        super.clearCanvas();
-        this.clearGLCanvas();
-    }
-
-    getCanvasImage() {
-        if (this.glCanvas && this.isCanvasUpdated()) {
-            const ctx = this.context;
-            if (maptalks.Browser.retina) {
-                ctx.save();
-                ctx.scale(1 / 2, 1 / 2);
-            }
-            // draw gl canvas on layer canvas
-            ctx.drawImage(this.glCanvas, 0, 0);
-            if (maptalks.Browser.retina) {
-                ctx.restore();
-            }
-        }
-        return super.getCanvasImage();
-    }
-
-    onRemove() {
-        this.removeGLCanvas();
-    }
-});
