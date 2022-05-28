100046527a934f24852fa9f2aa0265a76e493198
hu de yi <mehudeyi@163.com>
2022-05-28 16:42:31 +0800

    update ImageService for Provide independent functions (#6)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
  */
 import ImageMapLayer from './Layers/ImageMapLayer';
 import FeatureLayerService from './Layers/FeatureLayerService';
+import ImageLayerService from './Services/ImageService';
 import GeometryService from './Services/GeometryService';
 
-export { ImageMapLayer, FeatureLayerService, GeometryService }
+export { ImageMapLayer, FeatureLayerService, GeometryService, ImageLayerService }
