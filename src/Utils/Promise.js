ae331acddec59d9f0d9cd83c8a755cdf83a7735c
deyihu <mehudeyi@163.com>
2022-05-28 14:12:45 +0800

    update

--- src/Utils/Promise.js
+++ src/Utils/Promise.js
@@ -1,12 +1,2 @@
 import { Promise } from 'maptalks';
-
-// let promise;
-
-// if (typeof Promise !== 'undefined') {
-//     // built-in Promise
-//     promise = Promise;
-// } else {
-//     promise = Zousan;
-// }
-
 export default Promise;
